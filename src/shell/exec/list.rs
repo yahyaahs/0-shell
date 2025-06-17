eaf0be3b6cce5b80a3bb2055d44e8c1432f502d5
yahyahs <houssyahya@gmail.com>
2025-06-17 20:03:09 +0100

    dir file

--- src/shell/exec/list.rs
+++ src/shell/exec/list.rs
@@ -1,8 +1,22 @@
 pub use std::fs;
+use std::fs::Metadata;
+pub fn 
 
 pub fn ls(args: &Vec<String>){
     let paths =fs::read_dir(".").unwrap();
     for path in paths{
-        println!("paths {:?}", path);
+        match path{
+            Ok(name)=> 
+            match name.metadata(){
+                Ok(meta)=> if meta.is_dir(){
+                    println!("directory {:?}", name.path());
+                }else{
+                    println!("{:?}", name.file_name())
+                },
+                _=>println!("error"),
+
+            },
+            _=> println!("no read"),
+        }
     }
 }
