772dd0d3f65b8535bbe87bb820a48ff206d0d9c2
yahyahs <houssyahya@gmail.com>
2025-06-17 21:26:04 +0100

    data indexing

--- src/shell/exec/list.rs
+++ src/shell/exec/list.rs
@@ -1,21 +1,36 @@
 pub use std::fs;
+use std::{ffi::OsString, fs::DirEntry, io::Error};
+#[derive(Debug)]
+pub enum Types {
+    File(OsString),
+    Dir(OsString),
+    Executable(OsString),
+    Symlink(OsString),
+    NoSupported,
+    Error,
+}
+pub fn check_type(name : DirEntry)-> Types{
+    
+    match name.metadata(){
+        Ok(meta)=> 
+            if meta.is_dir(){
+                return Types::Dir(name.file_name());
+            } else if meta.is_file() {
+                return Types::File(name.file_name());
+            } else if meta.is_symlink(){
+                return Types::Symlink(name.file_name());
+            } else {
+                return Types::NoSupported;
+            },
+        _=>Types::Error,
+    }
 
+}
 pub fn ls(args: &Vec<String>){
     let paths =fs::read_dir(".").unwrap();
-    for path in paths{
-        match path{
-            Ok(name)=> 
-            match name.metadata(){
-                Ok(meta)=> if meta.is_dir(){
-                    println!("directory {:?}", name.path());
-                    println!("{:#?}", name.metadata().unwrap().permissions());
-                }else{
-                    println!("{:?}", name.file_name())
-                },
-                _=>println!("error"),
-
-            },
-            _=> println!("no read"),
-        }
+    let mut types = vec![];
+    for data in paths{
+        types.push(check_type(data.unwrap()));
     }
+    println!("types {:?}", types);
 }
