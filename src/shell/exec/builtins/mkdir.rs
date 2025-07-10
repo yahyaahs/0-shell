e9de0556e703010cc4877f5a34f5ddf5af2c893e
Ayamne <ouchchateaymane@gmail.cpm>
2025-07-11 00:09:19 +0100

    fixing when the user don't enter any args like write mkdir

--- src/shell/exec/builtins/mkdir.rs
+++ src/shell/exec/builtins/mkdir.rs
@@ -1,32 +1,32 @@
 use crate::shell::{Shell, parse::Cmd};
-use std::{fs::create_dir, io, path};
+use std::{fs::create_dir, io};
 use io::*;
 /*
     mkdir folder already existed =>  mkdir: Desktop: File exists
     mkdir folder existed and one not => mkdir: Desktop: File exists\n create the new one
     mkdir folder with not valid path => mkdir: jj: No such file or directory
 */
-pub fn mkdir(shell: &mut Shell, command:&Cmd) {
-    println!("{:?}",command.args);
+pub fn mkdir(_shell: &mut Shell, command:&Cmd) {
+    if command.args.len() == 0 {
+        println!("usage: mkdir directory_name ...");
+        return
+    };
     for f in &command.args {
         let folder_name : &String = f;
-        // let err : io::Error;
         create_dir(folder_name).unwrap_or_else(|error|{
-            // err = error;
             match error.kind() {
                 ErrorKind::NotFound => {
                     let not_found : Vec<&str>= f.split("/").collect();
                     println!("{}: {}: {}",command.exec,not_found[0],"No such file or directory");
+                    return
                 },
                 ErrorKind::AlreadyExists => {
                     let already_exist : Vec<&str>= f.split("/").collect();
                     println!("{}: {}: {}",command.exec,already_exist[0],"File exists");
-    
+                    return
                 }
-                _ => println!("{}: {}",command.exec,error)
+                _ => return
             }
-           
-            // err.clear();
         })
     }
    
